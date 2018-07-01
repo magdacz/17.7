631f72ac8264f26b0d197e6164b39db3bcaa5e98
Magda Czaicka-Chmielewska <czaicka.chmielewska@op.pl>
2018-07-01 14:47:42 +0200

    Add files

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  'GOOGLE_CLIENT_ID': '349936594902-uqhh1hkb045irgtitmkc40snn899a866.apps.googleusercontent.com',
+  'GOOGLE_CLIENT_SECRET': 'gaPFJnZLCyBNTNaV0zmKDhXY',
+  'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+}
